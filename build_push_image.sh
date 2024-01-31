015092fe4f987f2517e8f068ef44a6bdfdbe1d86
Yudha E <masbroumail@gmail.com>
2024-01-31 20:16:53 +0700

    .

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,16 +2,16 @@
 # Menentukan shell yang akan digunakan untuk menjalankan skrip
 
 # Membangun image Docker item-app:v1 dari Dockerfile yang terletak di direktori saat ini 
-docker build -t item-app:v1.1 .
+docker build -t item-app:v1 .
 
 # Menampilkan daftar image Docker yang ada di sistem
 docker images
 
 # Mengunggah docker image item-app ke Github Packages
-docker tag item-app:v1 ghcr.io/masbroustudio/item-app:v1.1
+docker tag item-app:v1 ghcr.io/masbroustudio/item-app:v1
 
 # Melakukan login ke GitHub Container Registry menggunakan token akses yang disimpan dalam variabel lingkungan $GH_PACKAGES_TOKEN
 echo $GH_PACKAGES_TOKEN | docker login ghcr.io -u masbroustudio --password-stdin
 
 # Mengunggah docker image item-app ke GitHub Container Registry
-docker push ghcr.io/masbroustudio/item-app:v1.1
+docker push ghcr.io/masbroustudio/item-app:v1
